992534e312c85d3052e8b0afd0135e4e167f6cac
Mark-James McDougall <markjames@hh-dev.com>
2021-12-31 07:41:28 -0500

    - Renamed project

--- BasketballSharp/Api.cs
+++ BasketballSharp/Api.cs
@@ -1,6 +1,28 @@
-﻿namespace BasketballSharp;
+﻿namespace NBASharp;
 
-public class Api
+/// <summary>
+/// The Api class holds all MLB Stats API endpoints that can be accessed from Baseball Sharp.
+/// </summary>
+public static class Api
 {
+    private static readonly string _baseUrl = "https://data.nba.net/prod/v1";
 
+    private static async Task<string> getResponse(string? endpoint)
+    {
+        using (var client = new HttpClient())
+        {
+            var return_message = new HttpResponseMessage();
+
+            try
+            {
+                return_message = await client.GetAsync(_baseUrl + (endpoint ?? "")).ConfigureAwait(false);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+
+            return await return_message.Content.ReadAsStringAsync();
+        }
+    }
 }
